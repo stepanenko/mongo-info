48651eaae27416d9d02d6fde453cf700e842e855
stepserg <serhiy.stepanenko@aiesec.net>
2018-10-27 12:01:14 +0300

    app component empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,13 @@
 
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Student } from './student';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  students: Student[];
-  activeStudent: Student;
-
-  ngOnInit() {
-    this.http.get('./assets/students.json')
-      .subscribe((data) => {
-        this.students = data as Student[];
-      });
-  }
-
-  selectStudent(student) {
-    this.activeStudent = student;
-  }
 }
